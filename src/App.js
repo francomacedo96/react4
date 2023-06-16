828955f10857197990352f138c7bc5e33c485f34
francomacedo96 <francomacedo@outlook.com>
2023-06-15 22:10:42 -0300

    img

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import logo from "./imagenes/logo192.png";
 
 function App() {
   return (
-    <div className="App">
-
+    <div className="aplicacion-tareas">
+      <div className='logo-contenedor'>
+        <img src={logo} className='logo' alt="" />
+      </div>
     </div>
   );
 }
